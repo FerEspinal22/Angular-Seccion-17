1fe4c175b1a3c9b6236d3eb28a4c8e1ad0dd9b6b
FerEspinal22 <ferespinal2001@gmail.com>
2023-10-16 22:55:51 -0600

    Avance hasta el video 236

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 5,
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
+
+  // // ? Primera forma de creae un formulario reactivo
+
+  // // * Crear formulario reactivo
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage : new FormControl(0),
+  // });
+
+  // ? Segunda forma de crear un formulario reactivo (Esta usa Fernando Herrera)
+
+  public myForm: FormGroup = this.fb.group({
+    name:      ['', [ Validators.required,  Validators.minLength(3) ] ],
+    price:     [0,  [ Validators.required,  Validators.min(0) ] ],
+    inStorage: [0,  [ Validators.required,  Validators.min(0) ] ],
+  })
+
+  constructor (private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm.reset(rtx5090)
+  }
+
+  onSave(): void {
+
+    if( this.myForm.invalid ) return;
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
+  }
 
 }
