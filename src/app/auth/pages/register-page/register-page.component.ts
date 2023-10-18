3de923f313e35d236fb114f74a0e72d83578ed5e
Fernando Josue Espinal Rios <fernando.espinal@senprende.hn>
2023-10-18 16:01:27 -0600

    Avances hasta el video 252

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { ValidatorsService } from 'src/app/shared/services/validators.service';
+// import * as CustomValidators from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
 })
 export class RegisterPageComponent {
+
+  
+
+  public myForm: FormGroup = this.fb.group({
+    name:            ['', [ Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern) ]],
+    // email:           ['', [ Validators.required, Validators.pattern(this.validatorsService.emailPattern) ], [ new EmailValidator() ]],
+    email:           ['', [ Validators.required, Validators.pattern(this.validatorsService.emailPattern) ], [ this.emailValidator ]],
+    userName:        ['', [ Validators.required, this.validatorsService.cantBeStrider ]],
+    password:        ['', [ Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [ Validators.required, ]],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidator
+  ) {}
+
+  isValidField(field: string) {
+    return this.validatorsService.isValidField( this.myForm, field );
+  }
+
+  onSubmit() {
+    // ? disparar que todos los formularios han sido tocados y hacer uso de nuestro id
+    this.myForm.markAllAsTouched();
+  }
 
 }
